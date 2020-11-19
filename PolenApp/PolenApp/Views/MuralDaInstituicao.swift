a831343673c357676bfc75f66bc3aa067bd678c7
Elaine <ecf2@cin.ufpe.br>
2020-11-19 12:00:37 -0300

    Ajustes no Banner do Mural

--- PolenApp/PolenApp/Views/MuralDaInstituicao.swift
+++ PolenApp/PolenApp/Views/MuralDaInstituicao.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 import SwiftUI
 
-struct BannerView: View{
+struct BannerView: View {
     
-    var body: some View{
+    var body: some View {
         ZStack(alignment: .top, content: {
             Image("Banner")
                 .resizable().aspectRatio(contentMode: .fit).frame(minWidth: 300,
@@ -20,46 +20,61 @@ struct BannerView: View{
                                                                   idealHeight: 400,
                                                                   maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
                                                                   alignment: .top)
-                .offset(x: 0, y: -200)
-            HStack{
-                Image("Adus")
-                    .resizable().aspectRatio(contentMode: .fit).frame(width: 120, height: 170, alignment: .top)
-                    .offset(x: 220, y: -140)
-                    .padding(20)
-                Text("São Paulo - SP")
-                    .font(.system(size: 14, weight: .medium, design: .default))
-                    .frame(width: 100, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .offset(x: 110, y: -90)
-                Image("Location")
-                    .resizable().aspectRatio(contentMode: .fit).frame(width: 20, height: 20, alignment: .top)
-                    .offset(x: -27, y: -92)
-                Image("Separador")
-                    .resizable().aspectRatio(contentMode: .fit).frame(width: 130, height: 190, alignment: .top)
-                    .offset(x: 20, y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
-                Image("PhoneIcon")
-                    .resizable().aspectRatio(contentMode: .fit).frame(width: 40, height: 40, alignment: .top)
-                    .offset(x: -200, y: -40)
-                Image("GloboIcon")
-                    .resizable().aspectRatio(contentMode: .fit).frame(width: 40, height: 40, alignment: .top)
-                    .offset(x: -200, y: -40)
-                Image("InstaIcon")
-                    .resizable().aspectRatio(contentMode: .fit).frame(width: 40, height: 40, alignment: .top)
-                    .offset(x: -200, y: -40)
-                
-                VStack{
-                    Text("Adus")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.leading)
-                        .padding([.top, .bottom, .trailing], 5)
-                        .offset(x: -300, y: -50)
-                    Text("Organização sem fins lucrativos que atua na integração social de refugiados e estrangeiros vítimas de migrações forçadas.")
-                        .font(.system(size: 12, weight: .regular, design: .default))
-                        .fontWeight(.regular)
-                        .padding()
-                        .frame(width: 273, height: 300, alignment: .leading)
-                        .offset(x: -220, y: -180)
+            // .offset(x: 0, y: -200)
+            VStack (alignment: .leading) {
+                HStack{
+                    Image("Adus")
+                        .resizable().aspectRatio(contentMode: .fit).frame(width: 120, height: 170)
+                        // .offset(x: 220, y: -140)
+                        //.padding(20)
+                    
+                    VStack{
+                        Text("Adus")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                            //.padding([.top, .bottom, .trailing], 5)
+                        // .offset(x: -300, y: -50)
+                        Text("Organização sem fins lucrativos que atua na integração social de refugiados e estrangeiros vítimas de migrações forçadas.")
+                            .font(.system(size: 12, weight: .regular, design: .default))
+                            .fontWeight(.regular)
+                            //.padding()
+                            .frame(width: 273, height: 100)
+                        //.offset(x: -220, y: -180)
+                    }
                 }
+                
+                VStack (){
+                    HStack{
+                        Text("São Paulo - SP")
+                            .font(.system(size: 14, weight: .medium, design: .default))
+                            .frame(width: 100, height: 2)
+                        //.offset(x: 110, y: -90)
+                        Image("Location")
+                            .resizable().aspectRatio(contentMode: .fit).frame(width: 20, height: 20)
+                        //.offset(x: -27, y: -92)
+                    }
+                
+                    Image("Separador")
+                        .resizable().aspectRatio(contentMode: .fit).frame(width: 30, height: 10)
+                    //.offset(x: 20, y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                    
+                    HStack{
+                        Image("PhoneIcon")
+                            .resizable().aspectRatio(contentMode: .fit).frame(width: 40, height: 40)
+                        //.offset(x: -200, y: -40)
+                        Image("GloboIcon")
+                            .resizable().aspectRatio(contentMode: .fit).frame(width: 40, height: 40)
+                        //.offset(x: -200, y: -40)
+                        Image("InstaIcon")
+                            .resizable().aspectRatio(contentMode: .fit).frame(width: 40, height: 40)
+                        //.offset(x: -200, y: -40)
+                    }
+                }
+                
+
+                
+                
             }
             
         }
@@ -81,7 +96,13 @@ struct SobreNos: Identifiable {
 }
 
 struct MuralDaInstituicaoView: View {
-    
+    init() {
+        //Configurações para NavigationBar
+        UINavigationBar.appearance().setBackgroundImage(UIImage(named: "Banner"), for: .default)
+        UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0.9688708186, green: 0.8066166639, blue: 0.3180420101, alpha: 1)
+        UINavigationBar.appearance().shadowImage = UIImage()
+        
+    }
     let colaboreList: [Colabore] = [
         Colabore(id:0, name: "Aulas de Português", description: "Precisamos de professores de português urgentes. Entre em contato conosco."),
         Colabore(id:2, name: "Aulas de Espanhol", description: "Precisamos de professores de espanhol urgentes. Entre em contato conosco."),
@@ -99,6 +120,7 @@ struct MuralDaInstituicaoView: View {
         SobreNos(id:4, image: "", title: "Auxílio Moradia", subtitle: "A ação é destinada a coletar doação de roupas, mantimentos, fraudas e outras necessidades que estão sendo detectadas pelo..."),
     ]
     
+    
     var body: some View{
         NavigationView{
             VStack(alignment: .leading){
@@ -106,11 +128,11 @@ struct MuralDaInstituicaoView: View {
                 Text("Colabore conosco")
                     .font(.headline)
                     .fontWeight(.bold)
-                    .offset(x: 20, y: -200)
+                //.offset(x: 20, y: -200)
                 
                 ScrollView(.horizontal){
                     VStack {
-                        LazyHStack{
+                        HStack{
                             ForEach (colaboreList){ colabore in  VStack{
                                 Text(colabore.name)
                                     .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
@@ -120,7 +142,7 @@ struct MuralDaInstituicaoView: View {
                                     .font(/*@START_MENU_TOKEN@*/.subheadline/*@END_MENU_TOKEN@*/)
                                     .multilineTextAlignment(.center)
                                     .lineLimit(4)
-                                    .padding(.all, 3.0)
+                                    //.padding(.all, 3.0)
                                 //                                Rectangle()
                                 //                                    .frame(width: 140, height: 100)
                                 //                                    .background(Color.white)
@@ -129,7 +151,8 @@ struct MuralDaInstituicaoView: View {
                                 //                                    .scaledToFit()
                                 
                                 
-                            }.frame(width: 300, height: 200)
+                            }
+                            .frame(width: 300, height: 200)
                             }
                         }
                     }
@@ -137,7 +160,7 @@ struct MuralDaInstituicaoView: View {
                 Text("Um pouco sobre nós")
                     .font(.headline)
                     .fontWeight(.bold)
-                    .offset(x: 20, y: -30)
+                //.offset(x: 20, y: -30)
                 ScrollView(.horizontal){
                     LazyHStack{
                         ForEach (sobreNosList){ sobreNos in
@@ -154,26 +177,33 @@ struct MuralDaInstituicaoView: View {
                                     .font(/*@START_MENU_TOKEN@*/.subheadline/*@END_MENU_TOKEN@*/)
                                     .multilineTextAlignment(.center)
                                     .lineLimit(4)
-                                    .padding(.all, 3.0)
+                                    //.padding(.all, 3.0)
                                 
                                 
-                            }.frame(width: 300, height: 200)
-                            .padding(2)
+                            }
+                            .frame(width: 300, height: 200)
+                            //padding(2)
                         }
                     }
                 }
             }
+
+              .navigationBarTitle("", displayMode: .inline)
+           
             
         }
+        
+    
+       
     }
     
 }
-
 
 #if DEBUG
 struct MuralView_Previews: PreviewProvider {
     static var previews: some View {
         MuralDaInstituicaoView()
+            .previewDevice("iPhone 11")
     }
 }
 #endif
